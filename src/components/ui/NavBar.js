256166b6359710a1b2b8282a99051e1e57e56aec
JuanMora <juanpablomora777@gmail.com>
2022-07-15 16:12:05 -0500

    nav con bootstrap

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,25 +1,48 @@
 import React from 'react';
 
-// import '../Styles/nav.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Nav (){
     
     return(
         <header>
-            <nav>
-            <div class="nav-bar">
-            
-                <div class="anav">
-                    <a href="/">Inicio</a>
-                    <a href="/">Productos</a>
-                    <a href="/">Planes</a>
-                    <a href="/">¿Quienes somos?</a>
-                    <button className='logout-button'>Cerrar sesión</button>
-                    
-                    
-                </div>
-            </div>
-        </nav>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                      <a class="navbar-brand" href="#">Navbar</a>
+                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Información</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Servicios
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Contacto</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            {/* <li><hr class="dropdown-divider"> </li> */}
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                                <button class="btn btn-outline-success" type="submit">Ingresar</button>
+                            </form>
+                            </div>
+                        </div>
+                </nav>
     </header> 
     );
 }
