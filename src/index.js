deb90a9a8b1284715f3d5874ea75e620674aebe4
JuanMora <juanpablomora777@gmail.com>
2022-07-15 14:47:00 -0500

    arreglo index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Nav from './components/login/NavBar';
+import Nav from './components/ui/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
