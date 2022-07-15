c878f367da2bed380a39c73f21434f0240097db0
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-07-15 18:00:04 -0500

    No se un culo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Nav from './components/ui/NavBar';
+import Footer from './components/footer/footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Nav />
+    <Footer/>
   </React.StrictMode>
 );
