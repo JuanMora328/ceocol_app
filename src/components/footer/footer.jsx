1529bd4e4c76be116bd65182edb3a9dde5439e06
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-07-15 14:43:15 -0500

    Footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../../styles/footer.css';
+
+export const Footer = () =>{
+
+    return (
+      
+        <div class="footer">
+            <div class="ceocol">
+                CEOCOL
+            </div>
+       
+        <div class="subfoot">
+        <a class="n" href="https://www.instagram.com/fitboxgympalmira/?hl=es"><input class="insta" type="button" /></a>
+        <a class="n" href="https://www.facebook.com/fitboxmovil"><input class="facebook" type="button" /></a>
+        <a class="n" href="https://api.whatsapp.com/send?phone=573166961391&app=facebook&entry_point=page_cta&fbclid=IwAR1si-fILeeElnx1rDuOWiUunXf2t4dffYQhMsOT69lcFJEZubZ0EwxXd4c"><input class="what" type="button" /></a>
+        
+        </div>
+        
+        <img class="fit1" src='https://i.ibb.co/x2F042s/Frame.png'/>
+
+        
+        
+      </div>
+        
+    )
+}
+export default Footer
