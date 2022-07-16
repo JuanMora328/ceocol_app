53919a6db978889aca4f7d1e4daf7b7edc984694
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-07-16 12:12:01 -0500

    Footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import '../../styles/footer.css';
-
+import '../../styles/footer.css';
 export const Footer = () =>{
 
     return (
       
         <div class="fixed-bottom">
            <div class="row g-0 bg-secondary">
-                <div class="col-6 bg-primary">
-                    <h1>CEOCOL</h1>
+                <div class="col-6 bg-primary align-items-center">
+                    <div class="container-fluid ">
+                            <h1>CEOCOL</h1>
+                            <p>Conductores Elegidos del Occidente de Colombia S.A.S<br></br>
+                            NIT: 900706021-0</p>
+                    </div>
+                    <div class="container-fluid bg-success">
+                    <div class="row g-0">
+                            <div class="col-md-2 bg-danger">.col-md-4</div>
+                            <div class="col-md-8 ms-auto">4021 Pagac Prairie</div>
+                        </div>
+                        <div class="row ">
+                            <div class="col-md-2 bg-danger">.col-md-4</div>
+                            <div class="col-md-8 ms-auto">795-820-3065</div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="col-6">
