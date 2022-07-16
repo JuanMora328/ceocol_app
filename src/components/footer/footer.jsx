ccc0ab7ef89bdcbca3eff478db7f6c6dfde737fb
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-07-15 23:29:31 -0500

    intento 2

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,15 +5,38 @@ export const Footer = () =>{
 
     return (
       
-        <div class="footer">
+        <div class="fixed-bottom">
            
-           <div class="container">
-                <div class="sub-container"><h1 >CEOCOL</h1></div>
-           </div>
+           <div class="row g-0 bg-secondary">
+           <div class="col-sm-6  bg-danger" >
+                <div class="g-col-6 g-col-md-4 ">
 
-           <div class="container">
-           <div class="sub-container"><h1>ASCENT</h1></div>
+              
+                    <div class="p-3 border "><h1 >CEOCOL</h1>
+                    <p > Conductores Elegidos del Occidente de Colombia S.A.S <br></br>
+                    NIT: 900706021-0</p>
+                    </div>
+                    <div ><p>4021 Pagac Prairie</p>
+                    
+                    
+                </div>
+
+                
+                </div>
+            
+              
+            
+            </div>
+                
+            <div class="col-sm-6  bg-primary">
+                    <div class=""><h1 >ASCENT</h1></div>
+                    
+                    </div>
+          
            </div>
+          
+           
+          
 
       </div>
         
