18ffb4864cd317878f054309724684fc92ab6bc3
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-07-15 23:31:24 -0500

    Revert "No se un culo"
    This reverts commit c878f367da2bed380a39c73f21434f0240097db0.

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,15 +6,21 @@ export const Footer = () =>{
     return (
       
         <div class="footer">
-           
-           <div class="container">
-                <div class="sub-container"><h1 >CEOCOL</h1></div>
-           </div>
+            <div class="ceocol">
+                CEOCOL
+            </div>
+       
+        <div class="subfoot">
+        <a class="n" href="https://www.instagram.com/fitboxgympalmira/?hl=es"><input class="insta" type="button" /></a>
+        <a class="n" href="https://www.facebook.com/fitboxmovil"><input class="facebook" type="button" /></a>
+        <a class="n" href="https://api.whatsapp.com/send?phone=573166961391&app=facebook&entry_point=page_cta&fbclid=IwAR1si-fILeeElnx1rDuOWiUunXf2t4dffYQhMsOT69lcFJEZubZ0EwxXd4c"><input class="what" type="button" /></a>
+        
+        </div>
+        
+        <img class="fit1" src='https://i.ibb.co/x2F042s/Frame.png'/>
 
-           <div class="container">
-           <div class="sub-container"><h1>ASCENT</h1></div>
-           </div>
-
+        
+        
       </div>
         
     )
