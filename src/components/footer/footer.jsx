dca1a3305bc761fb5a915d774f04962edff802c4
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-07-16 00:46:45 -0500

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,47 +6,17 @@ export const Footer = () =>{
     return (
       
         <div class="fixed-bottom">
-        <div class="footer">
-           
            <div class="row g-0 bg-secondary">
-           <div class="col-sm-6  bg-danger" >
-                <div class="g-col-6 g-col-md-4 ">
-
-              
-                    <div class="p-3 border "><h1 >CEOCOL</h1>
-                    <p > Conductores Elegidos del Occidente de Colombia S.A.S <br></br>
-                    NIT: 900706021-0</p>
-                    </div>
-                    <div ><p>4021 Pagac Prairie</p>
-                    
-                    
+                <div class="col-6 bg-primary">
+                    <h1>CEOCOL</h1>
                 </div>
-           <div class="container">
-                <div class="sub-container"><h1 >CEOCOL</h1></div>
-           </div>
 
-                
+                <div class="col-6">
+                <h1>ASCENT</h1>
                 </div>
-            
-              
-            
             </div>
-                
-            <div class="col-sm-6  bg-primary">
-                    <div class=""><h1 >ASCENT</h1></div>
-                    
-                    </div>
-          
-           <div class="container">
-           <div class="sub-container"><h1>ASCENT</h1></div>
-           </div>
-          
-           
-          
-
       </div>
-      </div>
-      </div>
+      
     )
 }
 export default Footer
